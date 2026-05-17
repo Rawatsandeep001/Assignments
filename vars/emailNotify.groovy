b7c746d00ffd5854c4badb803305939f36996cef
Rawatsandeep001 <sandeeprawat25691@gmail.com>
2026-05-17 22:23:13 +0530

    Refactor email notification function
    Removed the stage block from email notification function and simplified the email body.

--- vars/emailNotify.groovy
+++ vars/emailNotify.groovy
@@ -1,9 +1,7 @@
 def call(String status) {
-    stage('Email Notification') {
-        emailext (
-            subject: "Build ${status}",
-            body: "Build ${status} — Check Jenkins for details",
-            to: "sunraw541@gmail.com"
-        )
-    }
+    emailext(
+        subject: "Build ${status}",
+        body: "Check Jenkins",
+        to: "sunraw541@gmail.com"
+    )
 }

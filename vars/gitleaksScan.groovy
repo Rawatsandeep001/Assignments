7750a74733f012a41cd726d82db4788b4bf6aa15
Rawatsandeep001 <sandeeprawat25691@gmail.com>
2026-05-17 21:05:05 +0530

    Refactor Gitleaks scan function for clarity

--- vars/gitleaksScan.groovy
+++ vars/gitleaksScan.groovy
@@ -1,18 +1,5 @@
-def call() {
-    catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
-        sh '''
-        gitleaks detect \
-          --source . \
-          --report-format json \
-          --report-path gitleaks-report.json
-        '''
+def () {
+    stage('Gitleaks Scan') {
+        sh 'gitleaks detect --source . --report-format json --report-path gitleaks-report.json'
     }
-
-    sh '''
-    if [ ! -f gitleaks-report.json ]; then
-      echo "[]" > gitleaks-report.json
-    fi
-    '''
-
-    archiveArtifacts artifacts: 'gitleaks-report.json', fingerprint: true
 }
